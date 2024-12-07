149a1f9fd1ecfffd3bea597ef3cfadc98cb94454
Harshit Sahu <101046816+harshitsahu2311@users.noreply.github.com>
2024-12-07 18:22:11 +0530

    Update jenkins-script.sh

--- jenkins-script.sh
+++ jenkins-script.sh
@@ -1,28 +1,43 @@
 #!/bin/bash
 
-# install jenkins
+# Update the system
+sudo yum update -y
 
-sudo yum update
-sudo wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-sudo yum upgrade -y
-sudo amazon-linux-extras install java-openjdk11 -y
-sudo yum install jenkins -y
+# Install Git
+echo "Installing Git..."
+sudo apt install git -y
+
+# Install Jenkins
+echo "Installing jenkins..."
+
+sudo apt install default-jre -y
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install Jenkins -y
 sudo systemctl enable jenkins
 sudo systemctl start jenkins
 
-# install git
-sudo yum install git -y
+#Install Terraform
+echo "Installing Terraform...."
 
-# install terraform
+wget -O - https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install terraform -y
 
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum -y install terraform
+# Install kubectl
+echo "Installing kubectl..."
 
-# install kubectl
+sudo snap install kubectl --classic
+kubectl version --client
 
-sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
-sudo chmod +x ./kubectl
-sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+# Verify installations
+echo "Verifying installations..."
+git --version
+java -version
+jenkins --version
+terraform -version
+kubectl version --client
